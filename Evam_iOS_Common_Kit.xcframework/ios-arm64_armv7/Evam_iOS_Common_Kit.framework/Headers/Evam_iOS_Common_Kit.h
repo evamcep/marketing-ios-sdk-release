e9e8d6f6fc054509e8d5499882e83351f084ffa9
Cemalettin Serit <cemalettin@evam.com>
2021-08-12 16:18:00 +0300

    publish new version

---
+++ Evam_iOS_Common_Kit.xcframework/ios-arm64_armv7/Evam_iOS_Common_Kit.framework/Headers/Evam_iOS_Common_Kit.h
@@ -0,0 +1,18 @@
+//
+//  Evam_iOS_Common_Kit.h
+//  Evam_iOS_Common_Kit
+//
+//  Created by Sefa Ayçiçek on 25.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+#include "EvamSqliteManager.h"
+//! Project version number for Evam_iOS_Common_Kit.
+FOUNDATION_EXPORT double Evam_iOS_Common_KitVersionNumber;
+
+//! Project version string for Evam_iOS_Common_Kit.
+FOUNDATION_EXPORT const unsigned char Evam_iOS_Common_KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Evam_iOS_Common_Kit/PublicHeader.h>
+
+
